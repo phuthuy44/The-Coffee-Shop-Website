e9e9b048a24e72e978cf54c554e7289141aba067
Phuong vy <vy002359@gmail.com>
2022-08-06 19:57:05 +0700

    Responsive-Coffee-Shop-Website

---
+++ Readme.md
@@ -0,0 +1,14 @@
+#The Coffee Shop Website
+
+Create a complete responsive coffee shop one page website design tutorial for beginers using html css and vanilla javascript.
+
+The main feature of this website :
+ 1. Responsive header section with search box, navigation toggle and shopping cart box.
+ 2. Responsive Home and About section using css flexbox.
+ 3. Responsive Menu, Products, Review / Testimonial, and Blogs/News cards section using css grid.
+ 4. Responsive Contact form section using css grid.
+ 5. Responsive footer section.
+
+ Resource: https://www.youtube.com/watch?v=TVFu4-Kd4oM
+
+ key: #webdesign, #CoffeeShop, #FrontEnd, #Responsive.
---
+++ js/script.js
@@ -0,0 +1,32 @@
+let navbar = document.querySelector('.nav-coffee-bar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    cart.classList.remove('active');
+    searchFrom.classList.remove('active');
+}
+let searchFrom = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchFrom.classList.toggle('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+let cart = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchFrom.classList.remove('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    searchFrom.classList.remove('active');
+}
+document.querySelector('#home-coffee').onclick = () => {
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+    searchFrom.classList.remove('active');
+}
